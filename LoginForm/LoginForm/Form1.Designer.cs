0b3dd00e783fc57ca0d6e2f483bf281ee3ab81ab
Vi Nguyen <vivituong116@gmail.com>
2022-03-09 13:35:07 +0700

    A2 Them Label Mat khau, Textbox txtmatkhau

--- LoginForm/LoginForm/Form1.Designer.cs
+++ LoginForm/LoginForm/Form1.Designer.cs
@@ -30,46 +30,54 @@ namespace LoginForm
         private void InitializeComponent()
         {
             this.txtUserName = new System.Windows.Forms.TextBox();
-            this.password = new System.Windows.Forms.TextBox();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(155, 68);
-            this.txtUserName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtUserName.Location = new System.Drawing.Point(138, 54);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(253, 26);
+            this.txtUserName.Size = new System.Drawing.Size(225, 22);
             this.txtUserName.TabIndex = 0;
             this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
             // 
-            // password
+            // txtmatkhau
             // 
-            this.password.Location = new System.Drawing.Point(606, 68);
-            this.password.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(259, 26);
-            this.password.TabIndex = 1;
+            this.txtmatkhau.Location = new System.Drawing.Point(539, 54);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(231, 22);
+            this.txtmatkhau.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 68);
+            this.label1.Location = new System.Drawing.Point(44, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.Size = new System.Drawing.Size(75, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "UserName";
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(454, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mật khẩu";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.password);
+            this.Controls.Add(this.txtmatkhau);
             this.Controls.Add(this.txtUserName);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -80,8 +88,9 @@ namespace LoginForm
         #endregion
 
         private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox txtmatkhau;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
