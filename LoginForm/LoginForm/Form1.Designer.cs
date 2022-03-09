a5ff27d3cb5d8f7ac4d2407d27d8e1d2e3796736
Vi Nguyen <vivituong116@gmail.com>
2022-03-09 13:20:11 +0700

    A1 Tao Project va Textbox

---
+++ LoginForm/LoginForm/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace LoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.user = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(117, 55);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(225, 22);
+            this.user.TabIndex = 0;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(539, 54);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(231, 22);
+            this.password.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.user);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox password;
+    }
+}
+
